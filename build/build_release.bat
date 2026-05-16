15c8480569337804ded51d487c8b5542163fb493
Sokol <sokolenkonikita12.ru@mail.ru>
2026-05-16 10:58:50 +0300

    requirements

--- build/build_release.bat
+++ build/build_release.bat
@@ -1,2 +1,21 @@
 @echo off
-rem Release build script will be completed during the documentation/build stage.
+setlocal
+
+cd /d "%~dp0\.."
+
+set "PYTHON_EXE=python"
+if exist ".venv\Scripts\python.exe" set "PYTHON_EXE=.venv\Scripts\python.exe"
+
+"%PYTHON_EXE%" -m PyInstaller ^
+  --onefile ^
+  --windowed ^
+  --name TPR_Concession_Method ^
+  --distpath dist ^
+  --workpath build\pyinstaller ^
+  --specpath build ^
+  main.py
+
+if errorlevel 1 exit /b 1
+
+echo Release build created: dist\TPR_Concession_Method.exe
+endlocal
